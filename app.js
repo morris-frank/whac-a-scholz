0d48208868c8c76b5b3a4e4b9a81e267067f8836
Maurice Frank <maurice.frank@posteo.de>
2021-11-28 23:57:10 +0100

    init

---
+++ app.js
@@ -0,0 +1,52 @@
+const squares = document.querySelectorAll('.square')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
+
+function randomSquare() {
+  squares.forEach(square => {
+    square.classList.remove('scholz0')
+    square.classList.remove('scholz1')
+    square.classList.remove('scholz2')
+  })
+
+  let randomSquare = squares[Math.floor(Math.random() * 9)]
+  randomSquare.classList.add('scholz' + Math.floor(Math.random() * 3))
+
+  hitPosition = randomSquare.id
+}
+
+squares.forEach(square => {
+  square.addEventListener('mousedown', () => {
+    if (square.id == hitPosition) {
+      result++
+      score.textContent = result
+      hitPosition = null
+    }
+  })
+})
+
+function moveMole() {
+  timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
+
+function countDown() {
+ currentTime--
+ timeLeft.textContent = currentTime
+
+ if (currentTime == 0) {
+   clearInterval(countDownTimerId)
+   clearInterval(timerId)
+   alert('GAME OVER! Your final score is ' + result)
+ }
+
+}
+
+let countDownTimerId = setInterval(countDown, 1000)
+
